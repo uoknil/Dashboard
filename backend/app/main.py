703d6be5b2e1725ad7420a6eaca4c0423d7112de
CritinaPost <cristina.postoronca@stud.fh-campuswien.ac.at>
2026-05-04 00:06:24 +0200

    added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from .database import engine, SessionLocal, Base
 from . import models
@@ -6,6 +8,20 @@ from . import models
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Candida Auris Dashboard API")
+
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db():

94389b8ffe6ad37db95febfe877afc5ed100c1c8
CritinaPost <cristina.postoronca@stud.fh-campuswien.ac.at>
2026-05-29 11:01:53 +0200

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,8 @@ app = FastAPI(title="Candida Auris Dashboard API")
 
 
 origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]

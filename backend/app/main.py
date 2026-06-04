c11af2cadbaf5c0699fe337405aa36c679ce0148
CritinaPost <cristina.postoronca@stud.fh-campuswien.ac.at>
2026-06-04 12:48:47 +0200

    still trying to deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app = FastAPI(title="Candida Auris Dashboard API")
 
 origins = [
     "http://localhost:5173",
+    "https://remarkable-rejoicing-production-598d.up.railway.app/",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
